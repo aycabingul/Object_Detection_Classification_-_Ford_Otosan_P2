18a582e74166ca930509dfe8b2712ea9b0212828
aycabingul <aycaburcu99@gmail.com>
2021-09-09 00:19:32 +0300

    update

--- src/classification_src/Visualizing.py
+++ src/classification_src/Visualizing.py
@@ -9,18 +9,18 @@ class_num = []
 for folder in folders:
     train_files = os.listdir(train_path + '/' + folder)
     train_number.append(len(train_files))
-    
+    class_num.append(classes[int(folder)])
     
 # Sorting the dataset on the basis of number of images in each class
-zipped_lists = zip(train_number)
+zipped_lists = zip(train_number, class_num)
 sorted_pairs = sorted(zipped_lists)
 tuples = zip(*sorted_pairs)
-train_number = [ list(tuple) for tuple in  tuples]
+train_number, class_num = [ list(tuple) for tuple in  tuples]
 
 # Plotting the number of images in each class
 plt.figure(figsize=(21,10))  
-plt.bar(41, train_number)
-plt.xticks(41, rotation='vertical')
+plt.bar(class_num, train_number)
+plt.xticks(class_num, rotation='vertical')
 plt.tight_layout()
 plt.savefig("classes.png")
 plt.show()
